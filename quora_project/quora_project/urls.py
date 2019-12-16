98e8d39f99b78c2cf9d4609b0df0ce953921d88b
Vojta Janousek <janousek315@gmail.com>
2019-12-16 19:22:36 +0100

    fixed for travis.ci

--- quora_project/quora_project/urls.py
+++ quora_project/quora_project/urls.py
@@ -33,5 +33,6 @@ urlpatterns = [
 
     path('api-auth/', include('rest_framework.urls')),
     path('api/rest-auth/', include('rest_auth.urls')),
-    path('api/rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('api/rest-auth/registration/',
+         include('rest_auth.registration.urls')),
 ]

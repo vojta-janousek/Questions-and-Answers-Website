17b8a06de6db3b9af6609798689af246fefa7fde
Vojta Janousek <janousek315@gmail.com>
2019-12-17 23:56:31 +0100

    10 hours of debugging

--- quora_project/users/admin.py
+++ quora_project/users/admin.py
@@ -1,11 +1,46 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth import get_user_model
+from django.contrib.auth import forms
+
 from users.models import CustomUser
 
 
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
+        exclude = ()
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        exclude = ()
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            CustomUser.objects.get(username=username)
+        except CustomUser.DoesNotExist:
+            return username
+        raise forms.ValidationError(self.error_messages['duplicate_username'])
+
+
 class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    list_display = ['username', 'email', 'is_staff']
+    # list_display = ['username', 'email', 'is_staff']
+
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    fieldsets = (
+        (None, {'fields': [('username', 'password'),]}),
+        (('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                   'groups', 'user_permissions')}),
+        (('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        )
 
 
 admin.site.register(CustomUser, CustomUserAdmin)

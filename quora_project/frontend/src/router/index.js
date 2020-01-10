71bf4c9443bc6561868fa985f414152267678df0
Vojta Janousek <janousek315@gmail.com>
2020-01-10 21:09:12 +0100

    Vue Router

--- quora_project/frontend/src/router/index.js
+++ quora_project/frontend/src/router/index.js
@@ -15,7 +15,7 @@ const routes = [
     name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    // which is lazy-loaded when the route is visited.j
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
   }
@@ -23,7 +23,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
+  // base: process.env.BASE_URL,
   routes
 });
 
